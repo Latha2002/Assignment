4e1d0d6a33537e51aa6d36438a80dbacfebe771c
Latha2002 <135599645+Latha2002@users.noreply.github.com>
2023-06-06 19:15:02 +0530

    This is my First Commit

---
+++ Countword.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Countword
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+		int T = sc.nextInt();
+		for( int i = 0 ; i < T ; i++ ){
+		    int N = sc.nextInt();
+		    int M = sc.nextInt();
+		    System.out.println(N*M);
+		}
+		
+	}
+}
